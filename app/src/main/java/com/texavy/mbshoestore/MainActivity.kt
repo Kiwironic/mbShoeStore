7e97cc549ac6473d3428afefd7b038097807615d
MB <menabassily@hotmail.com>
2022-10-28 15:22:17 +0700

    Updates after receiving project feedback. Thank you very much for the constructive feedback.

--- app/src/main/java/com/texavy/mbshoestore/MainActivity.kt
+++ app/src/main/java/com/texavy/mbshoestore/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.texavy.mbshoestore
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import com.texavy.mbshoestore.databinding.ActivityMainBinding
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    /*seems like I need to do it another way for the activity. Signatures for onCreateView
+    here makes this method unsuitable for this activity
+    #todo: investigate how to inflate activities in details!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ActivityMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }*/
+
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
 }
 
 fun EditText.isValid(): Boolean {
